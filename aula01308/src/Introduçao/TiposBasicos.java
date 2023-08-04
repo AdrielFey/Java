7d7b14c9ae89b1450af41e45099244390a1808d3
AdrielFey <feyadrielluiz@gmail.com>
2023-08-03 21:56:25 -0300

    new
    |Tipoos basicos

---
+++ aula01308/src/Introduçao/TiposBasicos.java
@@ -0,0 +1,29 @@
+package Introduçao;
+
+public class TiposBasicos {
+/*
+1. Tipos Primitivos:
+
+Tipos Inteiros:
+
+byte|: 8bits , de -128 a 127
+short: 16 bits, de -32,768 a 32,767
+int: 32 bits, de -2,147,483,648 a 2,147,483,647
+long;64 bits, de  -9,223,372,036,854,775,808 a 9,2,036,854,775,807
+Tipos de Ponto Flutuante:
+
+float: 32bits, ~1.4e -45 a 3.4028235e38
+double: reprenta verdadeiro ou falso
+
+Tipo
+caracteres:
+string: 64bits, para armazenar uma cadeia de caracteress
+char: 16 bits, para armazenar carcteres Unicode
+ **/
+ int idade = 20; // um inteiro
+ double preco = 10.5; // um numero de ponto flutuante
+ char letra = 'a'; // um caracter
+ boolean isJavaFun = true; // um valor booleano
+ String nome = "John De"; // uma string(nao é um tipo primitivo)
+ int [] numeros = {1, 2, 3, 4, 5}; //um array de inteiros
+}
